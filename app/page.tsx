eab54d0a56f82f36f3bb26a41e127e8a50aece70
Hariprasad Natarajan <48129753+HariprasadKVN@users.noreply.github.com>
2024-03-03 17:51:55 +0530

    Added the context provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,157 +1,21 @@
-"use client";
 import Calendar from "@/components/Calendar";
 import LeaveInfo from "@/components/LeaveInfo";
-import { event } from "@/model/event";
-import EventContext from "@/store/eventContext";
-import { useState } from "react";
+import { EventContextProvider } from "@/store/eventContext";
 
 export default function Home() {
-  const [events, setEvents] = useState<event[]>([
-    { date: new Date(2024, 0, 1), description: "New Year", type: "fixed" },
-    { date: new Date(2024, 0, 26), description: "Republic Day", type: "fixed" },
-    { date: new Date(2024, 3, 9), description: "Ugadi", type: "fixed" },
-    { date: new Date(2024, 4, 1), description: "May Day", type: "fixed" },
-    {
-      date: new Date(2024, 7, 15),
-      description: "Independence Day",
-      type: "fixed",
-    },
-    {
-      date: new Date(2024, 9, 2),
-      description: "Gandhi Jayanti",
-      type: "fixed",
-    },
-    {
-      date: new Date(2024, 10, 1),
-      description: "Karnataka Rajyotsava",
-      type: "fixed",
-    },
-    { date: new Date(2024, 2, 25), description: "Holi", type: "optional" },
-    {
-      date: new Date(2024, 2, 29),
-      description: "Good Friday",
-      type: "optional",
-    },
-    {
-      date: new Date(2024, 5, 17),
-      description: "Eid al-Adha/Bakrid",
-      type: "optional",
-    },
-    {
-      date: new Date(2024, 7, 26),
-      description: "Janmashtami",
-      type: "optional",
-    },
-    { date: new Date(2024, 9, 31), description: "Diwali", type: "optional" },
-    {
-      date: new Date(2024, 11, 25),
-      description: "Christmas",
-      type: "optional",
-    },
-  ]);
-
-  const getUpdateElement = (event: {
-    date: Date;
-    description: string;
-    type?: "fixed" | "optional" | "optionalApplied" | "special" | "leave";
-    specialAllowed: boolean;
-    optionalAllowed: boolean;
-  }): event => {
-    switch (event.type) {
-      case undefined:
-        event.type = "leave";
-        break;
-      case "optional":
-        event.type = "optionalApplied";
-        break;
-      //From leave we can go to special, optionalApplied only if
-      //they are available. If non is available we will cancel the leave
-      case "leave":
-        event.type = "special";
-        break;
-      case "special":
-        event.type = "optionalApplied";
-        break;
-    }
-
-    return event;
-  };
-
-  const onDateClicked = (date: Date) => {
-    const event = events.find(
-      (n) => n.date.toDateString() === date.toDateString(),
-    );
-
-    if (event) {
-      //We will determine if we need to remove the event
-      //Get the available types of leaves
-      const specialAllowed =
-        events.filter((event) => event.type === "special").length < 1;
-      const optionalAllowed =
-        (event.type == "optional" || event.type == "optionalApplied") &&
-        events.filter((event) => event.type === "optionalApplied").length < 3;
-
-      if (
-        (event.type === "leave" && !(specialAllowed || optionalAllowed)) ||
-        (event.type === "special" && !optionalAllowed)
-      ) {
-        const updatedArray = events.filter(
-          (item, index) => item.date.toDateString() !== date.toDateString(),
-        );
-
-        setEvents(updatedArray);
-      } else if (event.type === "optionalApplied") {
-        const updateEvents = events.map((item, index) =>
-          item.date.toDateString() === date.toDateString()
-            ? {
-              date: item.date,
-              description: item.description,
-              type: "optional" as "fixed" | "optional" | "optionalApplied" | "special" | "leave"
-            }
-            : item,
-        );
-
-        setEvents(updateEvents);
-      } else if (event.type === "optional" && !optionalAllowed) {
-      } else {
-        const updateArray = events.map((item, index) =>
-          item.date.toDateString() === date.toDateString()
-            ? getUpdateElement({
-              date: item.date,
-              description: item.description,
-              type: item.type,
-              specialAllowed: specialAllowed,
-              optionalAllowed: optionalAllowed,
-            })
-            : item,
-        );
-
-        setEvents(updateArray);
-      }
-    } else {
-      console.log("push");
-      setEvents([...events, { date: date, description: "", type: "leave" }]);
-    }
-  };
-
   return (
-    <EventContext.Provider
-      value={{
-        events: events,
-        onDateClicked: onDateClicked,
-      }}
-    >      
-    <div className="flex flex-col">
-      <div className="flex flex-row p-2">
-        <div className="flex-none">
-          <LeaveInfo></LeaveInfo>
-        </div>        
-        <div className="flex-grow">
+    <EventContextProvider>
+      <div className="flex flex-col">
+        <div className="flex flex-row p-2">
+          <div className="flex-none">
+            <LeaveInfo></LeaveInfo>
+          </div>
+          <div className="flex-grow">
             <Calendar></Calendar>
           </div>
         </div>
         <div>Panel Task</div>
       </div>
-    </EventContext.Provider>
+    </EventContextProvider>
   );
 }
--- components/LeaveInfo.tsx
+++ components/LeaveInfo.tsx
@@ -1,10 +1,17 @@
+'use client'
+import { useContext } from "react";
+import EventContext from "@/store/eventContext";
+
 const LeaveInfo: React.FC = () => {
+    const { events } = useContext(EventContext);
+
     return (<div className="text-sm p-1 flex flex-col gap-1 border rounded-md border-slate-700">
-        <div className="rounded-md px-2 dark:bg-teal-300/50 bg-green-500/50">Fixed - 4</div>
-        <div className="rounded-md px-2 dark:bg-teal-300/70 bg-green-700/50">Optional - 5</div>
-        <div className="rounded-md px-2 dark:bg-sky-500/50 bg-sky-500/50">Leave - 7</div>
-        <div className="rounded-md px-2 dark:bg-violet-500/50 bg-violet-500/50">Special - 5</div>
-        <div className="rounded-md px-2 dark:bg-indigo-500/50 bg-indigo-500/50">Optional Applied - 9</div>
+        <div className="rounded-md px-2 dark:bg-teal-300/50 bg-green-500/50">
+            {`Fixed - ${events.filter((item) => item.type === "fixed").length}`}</div>
+        <div className="rounded-md px-2 dark:bg-teal-300/70 bg-green-700/50">{`Optional - ${events.filter((item) => item.type === "optional").length}`}</div>
+        <div className="rounded-md px-2 dark:bg-sky-500/50 bg-sky-500/50">{`Leave - ${events.filter((item) => item.type === "leave").length}`}</div>
+        <div className="rounded-md px-2 dark:bg-violet-500/50 bg-violet-500/50">{`Special - ${events.filter((item) => item.type === "special").length}`}</div>
+        <div className="rounded-md px-2 dark:bg-indigo-500/50 bg-indigo-500/50">{`Optional Applied - ${events.filter((item) => item.type === "optionalApplied").length}`}</div>
     </div>);
 };
 
