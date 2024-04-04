f130409b639ec291a6883793206bfe4012f39b36
Hariprasad Natarajan <48129753+HariprasadKVN@users.noreply.github.com>
2024-04-04 08:10:24 +0530

    fixing the table

--- components/dashboard/work-allocation.tsx
+++ components/dashboard/work-allocation.tsx
@@ -28,34 +28,42 @@ const WorkAllocation: React.FC<Props> = ({ isNewTaskAdded }) => {
     alert("clicked delete");
   };
 
+  console.log(tasks);
   return (
     <>
-      {/* <div className="m-5">
-        <button
-          className="flex-shrink-0 rounded border-4 border-teal-500 bg-teal-500 px-2 py-1 text-sm text-white hover:border-teal-700 hover:bg-teal-700"
-          type="button"
-        >
-          Add Task
-        </button>
-      </div> */}
-      <table className="... m-5 border-separate border border-slate-500">
+      <table>
+        <thead>
+          <tr><th>Task</th>
+            <th>Start By</th>
+            <th>Status</th>
+            <th>Action</th></tr>
+        </thead>
+        <tbody>
+          {tasks.map((task:IAllocation, index)=>(
+            <tr key={index}>
+              <td>{task.taskId} - {task.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* <table className="m-5 border-separate border border-slate-500">
         <thead>
           <tr>
-            <th className="... border border-slate-200">Task</th>
-            <th className="... border border-slate-200">Planned start date</th>
-            <th className="... border border-slate-200">Status</th>
-            <th className="... border border-slate-200">Action</th>
+            <th className="border border-slate-200">Task</th>
+            <th className="border border-slate-200">Planned start date</th>
+            <th className="border border-slate-200">Status</th>
+            <th className="border border-slate-200">Action</th>
           </tr>
         </thead>
         <tbody>
-          {tasks.map((task: IAllocation) => (
-            <tr key={task.id}>
-              <td className="... border border-slate-300">
+          {tasks.map((task: IAllocation, index) => (
+            <tr key={index}>
+              <td className="border border-slate-300">
                 {task.taskId} - {task.description}
               </td>
-              <td className="... border border-slate-300">{task.start}</td>
-              <td className="... border border-slate-300">{task.status}</td>
-              <td className="... border border-slate-300">
+              <td className="border border-slate-300">{task.start}</td>
+              <td className="border border-slate-300">{task.status}</td>
+              <td className="border border-slate-300">
                 <div style={{ display: "flex" }}>
                   {" "}
                   <span onClick={handleEdit} style={{ cursor: "pointer" }}>
@@ -71,7 +79,7 @@ const WorkAllocation: React.FC<Props> = ({ isNewTaskAdded }) => {
             </tr>
           ))}
         </tbody>
-      </table>
+      </table> */}
     </>
   );
 };
