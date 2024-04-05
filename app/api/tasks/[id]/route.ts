d2aa34478a4552e2f2a5afba5641fb839e29cb00
Hariprasad Natarajan <48129753+HariprasadKVN@users.noreply.github.com>
2024-04-05 13:25:43 +0530

    refactored the code with next directory structure

--- app/api/tasks/[id]/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } },
-) {
-  return Response.json({ message: `Test ${params.id}` });
-}
--- models/allocation.ts
+++ models/allocation.ts
@@ -1,8 +1,17 @@
 export default interface IAllocation {
-    id: number;
-    taskId: string;
-    description?: string;
-    start?: string;
-    end?: string;
-    status?: string;
-  }
+  id: number;
+  taskId: string;
+  description?: string;
+  start?: string;
+  end?: string;
+  status?: string;
+  _id?: string;
+  created_by?: string;
+  assigned_to?: string;
+  task_id?: string;
+  task_desc?: string;
+  estimate?: string;
+  start_date?: string;
+  end_date?: string;
+  current?:boolean;
+}
