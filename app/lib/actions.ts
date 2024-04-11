c48a9410670eaafa14b163dbf0cb963d2f7b17ea
Hariprasad <hariprasadkvn@hotmail.com>
2024-04-11 10:16:42 +0530

    check in to origin

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,23 +1,48 @@
 'use server';
 import { signIn } from '@/auth';
+import dbConnect from '@/lib/dbConnect';
+import Auth from '@/models/Auth';
 import { AuthError } from 'next-auth';
 
 export async function authenticate(
-    prevState: string | undefined,
-    formData: FormData,
-  ) {
-    try {
-      console.log('authenticate');
-      await signIn('credentials', formData);
-    } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.type) {
-          case 'CredentialsSignin':
-            return 'Invalid credentials.';
-          default:
-            return 'Something went wrong.';
-        }
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    console.log('authenticate');
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
       }
-      throw error;
     }
+    throw error;
   }
+}
+
+export async function addUser(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await dbConnect();
+    console.log(formData.get('name'));
+    const auth = await Auth.create({name:formData.get('name')});
+
+
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,16 @@
+'use client'
+import { useFormState } from "react-dom";
+import { addUser, authenticate } from '@/app/lib/actions';
+
 const Register: React.FC = () => {
-    return <>Register</>
+    const [errorMessage, dispatch] = useFormState(addUser, undefined);
+
+    return <>Register
+    <form action={dispatch}>
+        <input name="name" className="dark:bg-inherit border border-slate-400/50" type="text"/>
+        <button>Register</button>
+    </form>
+    </>
 }
 
 export default Register;
