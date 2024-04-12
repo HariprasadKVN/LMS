fd714b3743094fe87f92cb34645e2097709335f0
Hariprasad Natarajan <48129753+HariprasadKVN@users.noreply.github.com>
2024-04-12 11:24:34 +0530

    logout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,12 @@
 import { signOut } from '@/auth';
+
+const Dashboard: React.FC = () => {
     return <>This is your Dashboard
         <form action={async () => {
             'use server';
             await signOut();
         }}>
+            <button>
                 Sign out
             </button>
         </form>
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { AuthError } from "next-auth";
 import bcrypt from "bcrypt";
 import Auth from "@/models/userAuth";
 import dbConnect from "@/lib/dbConnect";
+import { rejects } from "assert";
 
 async function hashPassword(password: string): Promise<string> {
   const saltRounds = 10; // You can adjust the number of salt rounds as needed
@@ -47,15 +48,29 @@ export async function register(
       email: await hashPassword(getValue(formData.get("email"))),
       password: await hashPassword(getValue(formData.get("password"))),
     };
-    const email = await hashPassword(getValue(formData.get("email")));
-    const x = await Auth.findOne({ email: email });
+    const email = getValue(formData.get("email"));
+
+    const x = (await Auth.find()).filter((item) =>
+      bcrypt.compareSync(email, item.email),
+    );
     console.log(x);
 
-    if (await Auth.findOne({ email: email })) {
+    if (x && x.length === 1) {
+      console.log('user not created');
     } else {
+      console.log("user created");
       const x = await Auth.create(userAuth);
     }
   } catch (error) {
-    throw error;
+    // if (error instanceof AuthError) {
+    //   switch (error.type) {
+    //     case "CredentialsSignin":
+    //       return "Invalid credentials.";
+    //     default:
+    //       return "Something went wrong.";
+    //   }
+    // }
+    //throw error;
+    return "error";
   }
 }
