e6b37e71e5bc6f4acd8911f78f49671c6a96ac3b
GajananT <gajanant@emids.com>
2024-04-29 17:48:16 +0530

    Added code to display timesheet data and refactored logtime component

--- lib/timesheetActions.ts
+++ lib/timesheetActions.ts
@@ -1,4 +1,5 @@
 "use server";
+import { Effort } from "@/models/Effort";
 import Task from "@/models/Task";
 import { TimeSheet } from "@/models/TimeSheet";
 
@@ -40,4 +41,59 @@ export async function logTime(timeSheets: TimeSheet[]) {
 const convertDatetoKey = (date: Date) => {
   const key = `${date.getFullYear()}${(date.getMonth() + 1).toString().padStart(2, "0")}${date.getDate().toString().padStart(2, "0")}`;
   return key;
+};
+
+const convertDateKeyToDate = (date: string) => {
+  const dateVal = new Date(
+    Number(date.substring(0, 4)),
+    Number(date.substring(4, 6)) - 1,
+    Number(date.substring(6, 8)),
+  );
+  return dateVal;
+};
+
+const convertEffortObject = (efforts: any, keys: string[]) => {
+  let taskEfforts: Effort[] = [];
+  //const effortKeys = Object.keys(efforts);
+  keys.forEach((key) => {
+    const effort = efforts[key] ? efforts[key] : 0;
+    taskEfforts.push({ date: convertDateKeyToDate(key), effort: effort });
+  });
+  return taskEfforts;
+};
+
+export async function getInprogressTasks(currentWeek: Date) {
+  const keys = getWeekdayKeys(currentWeek);
+
+  let filteredTasks: TimeSheet[] = [];
+  try {
+    const tasks = await Task.find();
+    if (Array.isArray(tasks)) {
+      filteredTasks = tasks
+        .filter((task) => task.status === "in progress")
+        .map((task) => ({
+          taskId: task._id,
+          taskName: task.task_desc || "",
+          status: task.status,
+          effort: convertEffortObject(task.efforts, keys),
+        }));
+    } else {
+      console.error("Error fetching tasks:");
+    }
+  } catch (error) {
+    throw error;
+  }
+  return filteredTasks;
+}
+
+const getWeekdayKeys = (from: Date): string[] => {
+  const weekStart = new Date(from);
+  const weekdaysArr: string[] = [];
+  for (let i = 0; i < 7; i++) {
+    const day = new Date(weekStart);
+    day.setDate(weekStart.getDate() + i);
+    weekdaysArr.push(convertDatetoKey(day));
+  }
+
+  return weekdaysArr;
 };
