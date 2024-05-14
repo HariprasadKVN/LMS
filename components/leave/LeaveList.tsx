d9f2675c00cc1fb046690bd607ccfb59b7c17269
rushi <rtrushi2441@gmail.com>
2024-05-14 16:24:08 +0530

    added checks for null data and changes as per employee store get method

--- components/leave/LeaveList.tsx
+++ components/leave/LeaveList.tsx
@@ -34,54 +34,110 @@ const LeaveList: React.FC<Props> = ({
     fetchLeaveData();
   };
 
+  // return (
+  //   <>
+  //     {Object.entries(leaveData).map(([type, leaves], index) => (
+  //       <UCCard
+  //         key={index}
+  //         title={type}
+  //         subTitle={`Allotted-${utilizedLeavesCount[type].allotted} Utilized-${utilizedLeavesCount[type].utilized || 0} Balance-${utilizedLeavesCount[type].balance}`}
+  //       >
+  //         <UCTable className="text-center">
+  //           <UCTableHeader>
+  //             <UCTableRow>
+  //               <UCTableHeaderCell>Start Date</UCTableHeaderCell>
+  //               <UCTableHeaderCell>End Date</UCTableHeaderCell>
+  //               <UCTableHeaderCell>Day</UCTableHeaderCell>
+  //               <UCTableHeaderCell>Duration</UCTableHeaderCell>
+  //               <UCTableHeaderCell>Reason</UCTableHeaderCell>
+  //               <UCTableHeaderCell>Cancel</UCTableHeaderCell>
+  //             </UCTableRow>
+  //           </UCTableHeader>
+  //           <UCTableBody>
+  //             {leaves.map((leave, index) => {
+  //               return (
+  //                 <UCTableRow key={index}>
+  //                   <UCTableCell>{leave.startDate}</UCTableCell>
+  //                   <UCTableCell>{leave.endDate}</UCTableCell>
+  //                   <UCTableCell>{leave.noOfDays}</UCTableCell>
+  //                   <UCTableCell>{leave.duration}</UCTableCell>
+  //                   <UCTableCell>{leave.reason}</UCTableCell>
+  //                   <UCTableCell>
+  //                     <XMarkIcon
+  //                       title="Delete leave"
+  //                       className="h-4 w-4 cursor-pointer hover:text-red-700"
+  //                       onClick={() =>
+  //                         handleDeleteLeave(
+  //                           `leaves.${currentYear}.${type}.${leave.startDate}`,
+  //                         )
+  //                       }
+  //                     />
+  //                   </UCTableCell>
+  //                 </UCTableRow>
+  //               );
+  //             })}
+  //           </UCTableBody>
+  //         </UCTable>
+  //       </UCCard>
+  //     ))}
+  //   </>
+  // );
+
   return (
     <>
-      {Object.entries(leaveData).map(([type, leaves], index) => (
-        <UCCard
-          key={index}
-          title={type}
-          subTitle={`Allotted-${utilizedLeavesCount[type].allotted} Utilized-${utilizedLeavesCount[type].utilized || 0} Balance-${utilizedLeavesCount[type].balance}`}
-        >
-          <UCTable className="text-center">
-            <UCTableHeader>
-              <UCTableRow>
-                <UCTableHeaderCell>Start Date</UCTableHeaderCell>
-                <UCTableHeaderCell>End Date</UCTableHeaderCell>
-                <UCTableHeaderCell>Day</UCTableHeaderCell>
-                <UCTableHeaderCell>Duration</UCTableHeaderCell>
-                <UCTableHeaderCell>Reason</UCTableHeaderCell>
-                <UCTableHeaderCell>Cancel</UCTableHeaderCell>
-              </UCTableRow>
-            </UCTableHeader>
-            <UCTableBody>
-              {leaves.map((leave, index) => {
-                return (
-                  <UCTableRow key={index}>
-                    <UCTableCell>{leave.startDate}</UCTableCell>
-                    <UCTableCell>{leave.endDate}</UCTableCell>
-                    <UCTableCell>{leave.noOfDays}</UCTableCell>
-                    <UCTableCell>{leave.duration}</UCTableCell>
-                    <UCTableCell>{leave.reason}</UCTableCell>
-                    <UCTableCell>
-                      <XMarkIcon
-                        title="Delete leave"
-                        className="h-4 w-4 cursor-pointer hover:text-red-700"
-                        onClick={() =>
-                          handleDeleteLeave(
-                            `leaves.${currentYear}.${type}.${leave.startDate}`,
-                          )
-                        }
-                      />
-                    </UCTableCell>
+      {Object.keys(leaveData).length === 0 ? (
+        <p>No Data Found</p>
+      ) : (
+        <>
+          {Object.entries(leaveData).map(([type, leaves], index) => (
+            <UCCard
+              key={index}
+              title={type}
+              subTitle={`Allotted-${utilizedLeavesCount[type].allotted} Utilized-${utilizedLeavesCount[type].utilized || 0} Balance-${utilizedLeavesCount[type].balance}`}
+            >
+              <UCTable className="text-center">
+                <UCTableHeader>
+                  <UCTableRow>
+                    <UCTableHeaderCell>Start Date</UCTableHeaderCell>
+                    <UCTableHeaderCell>End Date</UCTableHeaderCell>
+                    <UCTableHeaderCell>Day</UCTableHeaderCell>
+                    <UCTableHeaderCell>Duration</UCTableHeaderCell>
+                    <UCTableHeaderCell>Reason</UCTableHeaderCell>
+                    <UCTableHeaderCell>Cancel</UCTableHeaderCell>
                   </UCTableRow>
-                );
-              })}
-            </UCTableBody>
-          </UCTable>
-        </UCCard>
-      ))}
+                </UCTableHeader>
+                <UCTableBody>
+                  {leaves.map((leave, index) => {
+                    return (
+                      <UCTableRow key={index}>
+                        <UCTableCell>{leave.startDate}</UCTableCell>
+                        <UCTableCell>{leave.endDate}</UCTableCell>
+                        <UCTableCell>{leave.noOfDays}</UCTableCell>
+                        <UCTableCell>{leave.duration}</UCTableCell>
+                        <UCTableCell>{leave.reason}</UCTableCell>
+                        <UCTableCell>
+                          <XMarkIcon
+                            title="Delete leave"
+                            className="h-4 w-4 cursor-pointer hover:text-red-700"
+                            onClick={() =>
+                              handleDeleteLeave(
+                                `leaves.${currentYear}.${type}.${leave.startDate}`,
+                              )
+                            }
+                          />
+                        </UCTableCell>
+                      </UCTableRow>
+                    );
+                  })}
+                </UCTableBody>
+              </UCTable>
+            </UCCard>
+          ))}
+        </>
+      )}
     </>
   );
+  
 };
 
 export default LeaveList;
