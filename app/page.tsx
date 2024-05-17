5beb5636519f59100f4e0865bf05ac577909eaab
Hariprasad Natarajan <48129753+HariprasadKVN@users.noreply.github.com>
2024-05-17 14:04:32 +0530

    removed the login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import dynamic from 'next/dynamic';
 
 const LoginPage: React.FC = () => {
-    const NoSSRLoading = dynamic(() => import('@/components/login/login'), { ssr: false })
+    const NoSSRLogin= dynamic(() => import('@/components/login/login'), { ssr: false })
 
     return (
         <div className="flex justify-center content-center">
             <div className="border border-blue-950/90 dark:border-teal-300 
                 rounded my-1 px-2 py-1">
-                <NoSSRLoading></NoSSRLoading>
+                <NoSSRLogin></NoSSRLogin>
             </div>
         </div>
     );
